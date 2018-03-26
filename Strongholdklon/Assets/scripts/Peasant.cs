1d33371064dca2b92ecd6ec8020a975c3e7a469c
QawsDK <peter.rasmusssen87@gmail.com>
2018-03-26 14:20:23 +0200

    løbske kasser

---
+++ Strongholdklon/Assets/scripts/Peasant.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Peasant : MonoBehaviour {
+
+    public int status = 0;
+    public int job = 0;
+
+    bool moving = false;
+
+    void Start () {
+        WanderAround();
+    }
+	
+	void Update () {
+        NavMeshAgent mNavMeshAgent = GetComponent<NavMeshAgent>();
+
+        if(!mNavMeshAgent.pathPending && moving) {
+            if(mNavMeshAgent.remainingDistance <= mNavMeshAgent.stoppingDistance) {
+                if(!mNavMeshAgent.hasPath || mNavMeshAgent.velocity.sqrMagnitude == 0f) {
+                   // Debug.Log("Done walking");
+                    moving = false;
+                    WanderAround();
+                }
+            }
+        }
+    }
+
+    void WanderAround() {
+        if(status != 0) {
+            return;
+        }
+
+        float x = Random.Range(-30, 30);
+        float z = Random.Range(-30, 30);
+        goTo(new Vector3(x,0,z));
+    }
+
+    public void goTo(Vector3 pos) {
+        moving = true;
+
+        GetComponent<UnityEngine.AI.NavMeshAgent>().SetDestination(pos);
+    }
+}
